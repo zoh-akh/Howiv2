258345295b2f876dffa0b329904245796d9b8b86
zoh-akh <36782624+zoh-akh@users.noreply.github.com>
2024-02-25 17:11:19 -0500

    first commit

---
+++ src/webparts/welcome/WelcomeWebPart.ts
@@ -0,0 +1,30 @@
+import { IPropertyPaneConfiguration } from "@microsoft/sp-property-pane";
+import { BaseClientSideWebPart } from "@microsoft/sp-webpart-base";
+import styles from "./WelcomeWebPart.module.scss";
+
+export interface IWelcomeWebPartProps {}
+
+export default class WelcomeWebPart extends BaseClientSideWebPart<IWelcomeWebPartProps> {
+  public render(): void {
+    this.domElement.innerHTML = `
+    <section class="${styles.welcome} ${
+      !!this.context.sdks.microsoftTeams ? styles.teams : ""
+    }">
+      <div class="${styles.welcome}">
+        <h3>Welcome, ${
+          this.context.pageContext.user.displayName.split(" ")[0]
+        }</h3>
+      </div>
+    </section>`;
+  }
+
+  protected onInit(): Promise<void> {
+    return Promise.resolve();
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [],
+    };
+  }
+}
